50ff68b2f4575d5b7b38de1a8748882ba355a6e7
mgbikash <mgbikash939@gmail.com>
2024-07-31 18:18:41 +0530

    app function extra part is deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      hello world first react Lorem ipsum dolor sit, amet consectetur
-      adipisicing elit. Excepturi labore libero quo aliquid aperiam aliquam,
-      nisi sint eaque officiis unde dicta quasi nulla repudiandae cum, iusto
-      dolorum possimus. In, mollitia.
-    </>
-  );
+  return <>hello world first react Lorem ipsum dolor sit, amet consectetur</>;
 }
 
 export default App;
