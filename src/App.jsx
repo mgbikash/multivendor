8d2c16817431dd66fd12a7bc46f8da6758a07dd0
mgbikash <mgbikash939@gmail.com>
2024-07-31 17:47:40 +0530

    lorem30 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <>hello world first react</>;
+  return (
+    <>
+      hello world first react Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Excepturi labore libero quo aliquid aperiam aliquam,
+      nisi sint eaque officiis unde dicta quasi nulla repudiandae cum, iusto
+      dolorum possimus. In, mollitia.
+    </>
+  );
 }
 
 export default App;
